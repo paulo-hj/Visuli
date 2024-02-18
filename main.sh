3dd6fc7f66394a89bbf482f87dcc6588879b84b9
Paulo Henrique <paulo.junior.ph@gmail.com>
2024-02-18 01:34:50 -0300

    Configura inicial do script

---
+++ main.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#-------------------------------------------------------------------#
+# AUTOR: Paulo Henrique
+# LINKEDIN: https://www.linkedin.com/in/paulo-henrique-oj/
+# GITHUB: https://github.com/paulo-hj
+#-------------------------------------------------------------------#
+
+# Configuracao
+refreshTimePage='60'
+titulo="visuli"
+versao="1.0"
+
+
