3ca239ae1a6fd5fb7690f7b3afc746181f71a70f
Paulo Henrique <paulo.junior.ph@gmail.com>
2024-02-18 15:02:08 -0300

    Alterada função diskInfo para melhor indentação e retorno dos resultados.

--- main.sh
+++ main.sh
@@ -11,15 +11,15 @@ titulo="Visuli"
 versao="1.0"
 
 htmlInicio(){
-    echo "<!DOCTYPE html>" >> visuli.html
-    echo "<html lang='pt-BR'>" >> visuli.html
-    echo "<head>" >> visuli.html
-    echo "<meta charset='UTF-8'>" >> visuli.html
-    echo "<meta http-equiv='refresh' content='$refreshTimePage'>" >> visuli.html
-    echo "<title>$titulo v$versao</title>" >> visuli.html
-    echo "</head>" >> visuli.html
-    echo "<body>" >> visuli.html
-}
+    echo "<!DOCTYPE html>"
+    echo "<html lang='pt-BR'>"
+    echo "<head>"
+    echo "<meta charset='UTF-8'>"
+    echo "<meta http-equiv='refresh' content='$refreshTimePage'>"
+    echo "<title>$titulo v$versao</title>"
+    echo "</head>"
+    echo "<body>"
+} >> visuli.html
 
 htmlFinal(){
       echo "<body/></html>" >> visuli.html
@@ -30,39 +30,53 @@ limparaArquivo(){
 }
 
 memoriaInfo(){
-    echo "<h2>Memória</h2>" >> visuli.html
-    free -mh >> visuli.html
-    echo "<hr>" >> visuli.html
-}
+    echo "<h2>Memória</h2>"
+    free -mh
+    echo "<hr>"
+} >> visuli.html
 
 diskInfo(){
-    echo "<h2>Espaço em disco</h2>" >> visuli.html
-    df -h >> visuli.html
-    echo "<hr>" >> visuli.html
-}
+    caminho=$(df -h 2>/dev/null | grep '/$' | awk '{print $1}')
+    valor=$(df -h 2>/dev/null | grep '/$' | awk '{print $5}' | tr -d %)
+    dicototal=$(df -H 2>/dev/null | grep -vE "^Filesystem|tmpfs|none|cdrom|udev")
+
+    echo "<h2>Disk</h2>"
+
+    if [ "$valor" -le 50 ]; then
+        echo "<p class='diskok'>BOM: $caminho com ${valor}% usado!</p>"
+    elif [ "$valor" -lt 90 ]; then
+        echo "<p class='diskwarning'>ALERTA: $caminho com ${valor}% usado!</p>"
+    elif [ "$valor" -ge 90 ]; then
+        echo "<p class='diskdanger'>DISCO CHEIO: $caminho com ${valor}% usado!</p>"
+    fi
+
+    echo "<pre>$dicototal</pre>"
+    echo "<hr>"
+} >> visuli.html
 
 loadAverage(){
-    echo "<h2>Load average</h2>" >> visuli.html
-    uptime | awk -F 'load average:' '{print $2}' >> visuli.html
-    echo "<hr>" >> visuli.html
-}
+    echo "<h2>Load average</h2>"
+    uptime | awk -F 'load average:' '{print $2}'
+    echo "<hr>"
+} >> visuli.html
 
 usuarios(){
-    echo "<h2>Usuário logados</h2>" >> visuli.html
-    echo "<p class='userson'>Tem <b>\"$(who | wc -l)\"</b> usuários logados.</p>" >> visuli.html
-    echo "<pre>$(who -s)</pre>" >> visuli.html
-    echo "<hr>" >> visuli.html
-}
+    echo "<h2>Usuário logados</h2>"
+    echo "<p class='userson'>Tem <b>\"$(who | wc -l)\"</b> usuários logados.</p>"
+    echo "<pre>$(who -s)</pre>"
+    echo "<hr>"
+} >> visuli.html
 
 systemInfo(){
   . /etc/os-release
-  echo "<h2>Informações do sistema</h2>" >> visuli.html
-  echo "<b>Hostname: $(hostname)</b>" >> visuli.html
-  echo "<b>Distribuition:</b> "$PRETTY_NAME" </br>" >> visuli.html
-  echo "<b>Kernel:</b> "$(uname -r)"<br>" >> visuli.html
-  echo "<b>Uptime:</b> "$(uptime -p | sed 's/up//')"</p>" >> visuli.html
-  echo "<hr>" >> visuli.html
-}
+  echo "<h2>Informações do sistema</h2>"
+  echo "<b>Hostname: $(hostname)</b>"
+  echo "<b>Distribuition:</b> "$PRETTY_NAME" </br>"
+  echo "<b>Kernel:</b> "$(uname -r)"<br>"
+  echo "<b>Uptime:</b> "$(uptime -p | sed 's/up//')"</p>"
+  echo "<hr>" 
+} >> visuli.html
+
 
 limparaArquivo
 htmlInicio
