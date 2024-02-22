084e80b880e2292971ef37cb62e851e6df6a2b4a
Paulo Henrique <paulo.junior.ph@gmail.com>
2024-02-21 23:48:43 -0300

    Adicioando script para escolher portas que devem ser verificadas se estão abertas, também foi automatizado o processo básico de instalação.

--- visuli-conf.sh
+++ visuli-conf.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+echo "Bem-vindo à instalação do Visuli!"
+
+# Solicitar ao usuário as portas que deseja verificar se estão abertas
+read -p "Por favor, insira as portas que deseja verificar se estão abertas (separadas por espaço): " portas
+
+# Modificar o arquivo visuli-main para incluir as portas a serem verificadas
+sed -i "s/\(portas=\"\)[^\"]*/\1$portas/" visuli-main
+
+chmod +x visuli-main
+sudo cp visuli-main /usr/local/bin/
+
+sudo mkdir -p /var/www/html/visuli-main
